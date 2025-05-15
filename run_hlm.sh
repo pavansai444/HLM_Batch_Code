6350fce7a9b4b2db4a12f390989bebb6327c0cb3
pavansai444 <98742800+pavansai444@users.noreply.github.com>
2025-05-15 23:22:33 +0530

    Update run_hlm.sh

--- run_hlm.sh
+++ run_hlm.sh
@@ -4,9 +4,10 @@
 batch_sizes=(8 16 32)
 lrs=(1e-6 3e-6 5e-6 1e-5 3e-5 5e-5 1e-4 3e-4 5e-4 1e-3 3e-3 5e-3)
 # Arrays of encoder and decoder models
-encoder_models=("google-bert/bert-base-multilingual-uncased" "FacebookAI/xlm-roberta-base")
-decoder_models=("google/gemma-2-2b" "meta-llama/Llama-3.2-3B" "sarvamai/sarvam-1")
-
+# encoder_models=("google-bert/bert-base-multilingual-uncased" "FacebookAI/xlm-roberta-base")
+# decoder_models=("google/gemma-2-2b" "meta-llama/Llama-3.2-3B" "sarvamai/sarvam-1")
+encoder_models=("FacebookAI/xlm-roberta-base")
+decoder_models=("google/gemma-2-2b")
 for batch_size in "${batch_sizes[@]}"; do
     for lr in "${lrs[@]}"; do
         for encoder_model in "${encoder_models[@]}"; do
